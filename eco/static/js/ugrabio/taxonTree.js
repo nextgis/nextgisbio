c5a9413c1bb716517cf564216a0dc31146110db4
Ivan Kavaliou (karavanjo) <jownkiv@gmail.com>
2013-11-07 08:46:06 +0300

    query cards by checkbox click was implemented

--- eco/static/js/ugrabio/taxonTree.js
+++ eco/static/js/ugrabio/taxonTree.js
@@ -1,8 +1,7 @@
 define(['dojo/dom', 'dojo/on', 'dojo/topic', 'cbtree/Tree', 'cbtree/model/FileStoreModel',
     'cbtree/store/FileStore', 'cbtree/extensions/TreeStyling', 'cbtree/store/Hierarchy',
-    'cbtree/model/TreeStoreModel', 'dojo/domReady!'],
-    function (dom, on, topic, cbTree, FileStoreModel, FileStore, TreeStyling, Hierarchy, TreeStoreModel) {
-        var taxonsTree;
+    'cbtree/model/TreeStoreModel', 'dojo/_base/event', 'dojo/domReady!'],
+    function (dom, on, topic, cbTree, FileStoreModel, FileStore, TreeStyling, Hierarchy, TreeStoreModel, event) {
         var store = new FileStore({
             url: "tree/taxons",
             basePath: '.',
@@ -17,7 +16,7 @@ define(['dojo/dom', 'dojo/on', 'dojo/topic', 'cbtree/Tree', 'cbtree/model/FileSt
             iconAttr: "icon"
         });
 
-        taxonsTree = new cbTree({
+        var taxonsTree = new cbTree({
             model: model,
             icon: { iconClass: "indentIcon", indent: false },
             branchIcons: true,
@@ -26,5 +25,37 @@ define(['dojo/dom', 'dojo/on', 'dojo/topic', 'cbtree/Tree', 'cbtree/model/FileSt
 
         taxonsTree.placeAt(dom.byId('leftCol'));
         taxonsTree.startup();
+
+        var onTaxonSelectedChanged = function () {
+            var id,
+                ids = [],
+                selectedTaxons = store.query({checked: true});
+            for (var i = 0, count = selectedTaxons.length; i < count; i++) {
+                id = selectedTaxons[i].path.split('/').pop();
+                if (id === '.') {
+                    id = 'root'
+                } else {
+                    id = 'taxon_' + id
+                }
+                ids.push(id);
+            }
+            topic.publish('taxon/selected/changed', ids);
+        };
+
+        taxonsTree.on("click", function (item, node, evt) {
+            onTaxonSelectedChanged();
+
+            if (item.name === '.') {
+                if (item.checked) {
+                    taxonsTree.collapseAll();
+                } else {
+                    taxonsTree._expandNode(node);
+                }
+            } else {
+                taxonsTree._collapseNode(node);
+            }
+
+        });
+
         return taxonsTree;
     })
