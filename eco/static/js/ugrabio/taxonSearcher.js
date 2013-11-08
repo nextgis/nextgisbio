c2b34a60c5872d81677b3abfc73b77c9a683cff4
Ivan Kavaliou (karavanjo) <jownkiv@gmail.com>
2013-11-08 04:31:22 +0300

    relation with Tree while firing select of taxon event was implemented

--- eco/static/js/ugrabio/taxonSearcher.js
+++ eco/static/js/ugrabio/taxonSearcher.js
@@ -1,20 +1,26 @@
-define(['dojo/ready', 'dijit/form/ComboBox'], function(ready, ComboBox) {
-    ready(function(){
-        var comboBox = new ComboBox({
-            id: "search",
-            name: "state",
-            value: "",
-            store: taxonStore,
-            searchAttr: "name",
-            pageSize:10,
-            queryExpr:'${0}',
-            style: "width: 50%;"
-        }, "search");
+define(['dojo/ready', 'dijit/form/ComboBox', 'dojox/data/QueryReadStore', 'dojo/on', 'dojo/request/xhr', 'ugrabio/taxonTree', 'dojo/DeferredList'],
+    function (ready, ComboBox, QueryReadStore, on, xhr, taxonTree, DeferredList) {
+        ready(function () {
+            var taxonStore = new QueryReadStore({
+                url: '/taxon/filter'
+            });
 
-        comboBox.on('change', function(newValue) {
+            var comboBox = new ComboBox({
+                id: "search",
+                name: "state",
+                value: "",
+                store: taxonStore,
+                searchAttr: "name",
+                pageSize: 10,
+                queryExpr: '${0}',
+                style: "width: 50%;",
+                autoComplete: false
+            }, "search");
 
-        });
-    });
+            comboBox.watch('item', function (what, oldVal, newVal) {
+                taxonTree.selectTaxon(newVal.i.id);
+            });
 
-    return { }
-})
+            return comboBox;
+        })
+    })
