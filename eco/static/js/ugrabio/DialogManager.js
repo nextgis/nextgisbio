e7e2a448be18ee79919048605dd01b405f2d9af9
karavanjo <jownkiv@gmail.com>
2013-11-20 08:30:12 +0300

    DialogManager for creating dialog forms was implemented

---
+++ eco/static/js/ugrabio/DialogManager.js
@@ -0,0 +1,43 @@
+define([
+    'dojo/_base/array',
+    'dojo/on',
+    'dojo/topic',
+    'dijit/MenuBar',
+    'dijit/PopupMenuBarItem',
+    'dijit/Menu',
+    'dijit/MenuItem',
+    'dijit/DropDownMenu',
+    'ugrabio/Forms',
+    'ugrabio/Dialog',
+    'dojo/domReady!'
+], function (array, on, topic, MenuBar, PopupMenuBarItem, Menu, MenuItem, DropDownMenu, Forms, Dialog) {
+    topic.subscribe('open/form', function (formId) {
+        var formTemplate = Forms.forms[formId],
+            listElementsId = formTemplate.elements,
+            formElements = [];
+
+        array.forEach(listElementsId, function (elementName) {
+            if (!Forms.elements[elementName]) {
+                console.log(elementName + ' element not found');
+            } else {
+                var elementTemplate = Forms.elements[elementName];
+
+                if (typeof elementTemplate === 'function') {
+                    formElements.push(elementTemplate());
+                } else {
+                    var params = [];
+                    array.forEach(elementTemplate.params, function (paramName) {
+                        params.push(formTemplate[paramName]);
+                    });
+                    formElements.push(elementTemplate.action.apply(undefined, params));
+                }
+            }
+
+        });
+
+        new Dialog({
+            title: 'Создать новую карточку',
+            elements: formElements
+        }).show();
+    });
+});
