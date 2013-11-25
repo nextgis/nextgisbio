16633eb5bd0a767056c3ff8452c91fd16155d090
karavanjo <jownkiv@gmail.com>
2013-11-25 03:11:21 +0300

    submit form button was added to Dialog

--- eco/static/js/ugrabio/Dialog.js
+++ eco/static/js/ugrabio/Dialog.js
@@ -2,39 +2,61 @@ define([
     'dojo/_base/declare',
     'dojo/_base/array',
     'dojo/_base/lang',
+    'dojo/_base/window',
+    'dojo/dom',
+    'dojo/dom-construct',
+    'dojo/dom-form',
+    'dojo/request/xhr',
     'dijit/_WidgetBase',
     'dijit/_TemplatedMixin',
     'dijit/_WidgetsInTemplateMixin',
-    'dijit/Dialog',
+    'dojox/layout/FloatingPane',
     'dijit/form/Form',
-    'dojox/layout/TableContainer'
-], function (declare, array, lang, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, Dialog, Form, TableContainer) {
+    'dojox/layout/TableContainer',
+    'dijit/form/Button'
+
+], function (declare, array, lang, win, dom, domConstruct, domForm, xhr, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, FloatingPane, Form, TableContainer, Button) {
+
+    var id_counter = 0;
 
     return declare([_WidgetBase], {
 
         constructor: function (options) {
             declare.safeMixin(this, options);
 
+            if (typeof this.submit === 'undefined') {
+                this.submit = true;
+            }
+
+            if (!this.style) {
+                this.style = 'position:absolute;top:55px;left:5px;height:300px;border:1px solid #759dc0;padding:5px;z-index:9999;overflow-x:hidden;';
+            }
+
             if (this.elements) {
-                this._buildForm(this.elements);
+                this._buildForm(this.elements, this.formSettings);
             } else {
                 throw ReferenceError;
             }
 
-            this._dialog = new Dialog({
+            var floatingDiv = domConstruct.create('div', {id: 'dialogFloat_' + id_counter++}, win.body());
+            this._dialog = new FloatingPane({
                 title: this.title,
                 content: this._form,
-                style: 'max-height:300px; overflow-y:scroll;'
-//                class: "non-modal"
-            });
+                resizable: true,
+                dockable: false,
+                maxable: true,
+                style: this.style
+            }, floatingDiv);
         },
 
-        _buildForm: function (elements) {
-            this._form = new Form();
+        _buildForm: function (elements, formSettings) {
+            this._form = new Form(formSettings);
 
             this._layout = new TableContainer({
                 showLabels: true,
-                orientation: 'horiz'
+                orientation: 'horiz',
+                style: 'width: 100%, margin: 5px;'
+
             });
 
             array.forEach(elements, lang.hitch(this, function (element) {
@@ -43,13 +65,38 @@ define([
 
             this._layout.placeAt(this._form.containerNode);
             this._layout.startup();
+
+            if (this.submit) {
+                var form = this._form,
+                    dialog = this._dialog;
+                new Button({
+                    label: 'Сохранить',
+                    style: 'margin: 10px 0 5px 5px;',
+                    onClick: function () {
+                        if (form.validate()) {
+                            var action = application_root + form.action,
+                                json_form = domForm.toObject(form.domNode),
+                                dialog = dialog;
+                            xhr.post(action, {
+                                data: json_form,
+                                handleAs: 'json'
+                            }).then(function (data) {
+                                    alert('Данные успешно обновлены!');
+                                    dialog.close();
+                                }, function (error) {
+                                    alert('Извините, произошла ошибка, попробуйте еще раз.');
+                                });
+                        }
+                    }
+                }).placeAt(this._form.containerNode);
+            }
         },
 
 
         show: function () {
             this.inherited(arguments);
             this._form.startup();
-            this._dialog.show();
+            this._dialog.startup();
         }
     });
 });
