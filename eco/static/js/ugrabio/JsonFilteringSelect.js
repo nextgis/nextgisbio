39c5b2d8709410656ee003c7c39e37d88214f620
karavanjo <jownkiv@gmail.com>
2013-11-25 03:36:17 +0300

    styling code

--- eco/static/js/ugrabio/JsonFilteringSelect.js
+++ eco/static/js/ugrabio/JsonFilteringSelect.js
@@ -2,23 +2,28 @@ define([
     'dojo/_base/declare',
     'dojo/_base/array',
     'dojo/_base/lang',
+    'dojo/aspect',
     'dijit/_WidgetBase',
     'dijit/_TemplatedMixin',
     'dijit/_WidgetsInTemplateMixin',
-    'dojo/store/JsonRest',
+    'dojox/data/QueryReadStore',
     'dijit/form/FilteringSelect',
     'dojo/domReady!'
-], function (declare, array, lang, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, JsonRest, FilteringSelect) {
-
-
-
+], function (declare, array, lang, aspect, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, QueryReadStore, FilteringSelect) {
     return declare(FilteringSelect, {
         constructor: function (options) {
             declare.safeMixin(this, options);
             if (this.url) {
-                this.store = new JsonRest({
-                    target: application_root + this.url
+                this.store = new QueryReadStore({
+                    url: application_root + this.url
                 });
+                // todo: add empty value to store
+//                aspect.before(this.store, '_xhrFetchHandler', function(a,b) {
+//                    var empty = this._items[0] ? this._items[0] : {};
+//                    empty.n = -1;
+//                    empty.i = {id: -1, name: ''};
+//                    this._items.unshift(empty);
+//                });
             } else {
                 console.log('JsonFilteringSelect: options.url is not defined');
             }
--- eco/static/js/ugrabio/TaxonSearcher.js
+++ eco/static/js/ugrabio/TaxonSearcher.js
@@ -1,26 +1,32 @@
-define(['dojo/ready', 'dijit/form/ComboBox', 'dojox/data/QueryReadStore', 'dojo/on', 'dojo/request/xhr', 'ugrabio/TaxonTree'],
-    function (ready, ComboBox, QueryReadStore, on, xhr, TaxonTree) {
-        ready(function () {
-            var taxonStore = new QueryReadStore({
-                url: application_root + '/taxon/filter'
-            });
+define([
+    'dojo/ready',
+    'dijit/form/ComboBox',
+    'dojox/data/QueryReadStore',
+    'dojo/on',
+    'dojo/request/xhr',
+    'ugrabio/TaxonTree'
+], function (ready, ComboBox, QueryReadStore, on, xhr, TaxonTree) {
+    ready(function () {
+        var taxonStore = new QueryReadStore({
+            url: application_root + '/taxon/filter'
+        });
 
-            var comboBox = new ComboBox({
-                id: "search",
-                name: "state",
-                value: "",
-                store: taxonStore,
-                searchAttr: "name",
-                pageSize: 10,
-                queryExpr: '${0}',
-                style: "width: 50%;",
-                autoComplete: false
-            }, "search");
+        var comboBox = new ComboBox({
+            id: "search",
+            name: "state",
+            value: "",
+            store: taxonStore,
+            searchAttr: "name",
+            pageSize: 10,
+            queryExpr: '${0}',
+            style: "width: 50%;",
+            autoComplete: false
+        }, "search");
 
-            comboBox.watch('item', function (what, oldVal, newVal) {
-                TaxonTree.selectTaxon(newVal.i.id);
-            });
+        comboBox.watch('item', function (what, oldVal, newVal) {
+            TaxonTree.selectTaxon(newVal.i.id);
+        });
 
-            return comboBox;
-        })
+        return comboBox;
     })
+})
