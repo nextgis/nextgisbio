9701dc92672276fedd6c24203ea2ea1993ed48d0
karavanjo <jownkiv@gmail.com>
2013-12-06 12:36:01 +0300

    Export AMD was renamed to WindowManager

--- eco/static/js/ugrabio/WindowManager.js
+++ eco/static/js/ugrabio/WindowManager.js
@@ -2,17 +2,14 @@ define([
     'dojo/_base/array',
     'dojo/on',
     'dojo/topic',
-    'dijit/MenuBar',
-    'dijit/PopupMenuBarItem',
-    'dijit/Menu',
-    'dijit/MenuItem',
-    'dijit/DropDownMenu',
-    'ugrabio/Forms',
-    'ugrabio/Dialog',
     'dojo/domReady!'
-], function (array, on, topic, MenuBar, PopupMenuBarItem, Menu, MenuItem, DropDownMenu, Forms, Dialog) {
-    var openWindow;
-    openWindow = function (url) {
+], function (array, on, topic) {
+
+    if (!ugrabio.is_auth) {
+        return false;
+    }
+
+    var openWindow = function (url) {
         window.open(url);
     };
 
