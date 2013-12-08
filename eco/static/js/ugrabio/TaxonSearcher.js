1dc87fcece0a90b68cb1eb311e18e459599cd008
karavanjo <jownkiv@gmail.com>
2013-12-08 19:59:31 +0300

    TaxonSearcher was changed to widget similar style

--- eco/static/js/ugrabio/TaxonSearcher.js
+++ eco/static/js/ugrabio/TaxonSearcher.js
@@ -1,32 +1,34 @@
-define([
-    'dojo/ready',
+define('ugrabio/TaxonSearcher', [
+    'dojo/_base/declare',
     'dijit/form/ComboBox',
-    'dojox/data/QueryReadStore',
-    'dojo/on',
-    'dojo/request/xhr',
-    'ugrabio/TaxonCbTree'
-], function (ready, ComboBox, QueryReadStore, on, xhr, TaxonCbTree) {
-    ready(function () {
-        var taxonStore = new QueryReadStore({
-            url: application_root + '/taxon/filter'
-        });
+    'dojox/data/QueryReadStore'
+], function (declare, ComboBox, QueryReadStore) {
+    var TaxonSearcher = declare('TaxonSearcher', [], {
+        _store: null,
+        _comboBox: null,
 
-        var comboBox = new ComboBox({
-            id: "search",
-            name: "state",
-            value: "",
-            store: taxonStore,
-            searchAttr: "name",
-            pageSize: 10,
-            queryExpr: '${0}',
-            style: "width: 50%;",
-            autoComplete: false
-        }, "search");
+        constructor: function (Tree) {
+            this._store = new QueryReadStore({
+                url: application_root + '/taxon/filter'
+            });
 
-        comboBox.watch('item', function (what, oldVal, newVal) {
-            TaxonCbTree.selectTaxon(newVal.i.id);
-        });
+            this._comboBox = new ComboBox({
+                id: "search",
+                name: "state",
+                value: "",
+                store: this._store,
+                searchAttr: "name",
+                pageSize: 10,
+                queryExpr: '${0}',
+                style: "width: 50%;",
+                autoComplete: false
+            }, "search");
 
-        return comboBox;
-    })
-})
+            this._comboBox.watch('item', function (what, oldVal, newVal) {
+                Tree.selectTaxon(newVal.i.id);
+            });
+        }
+    });
+
+    return TaxonSearcher;
+});
