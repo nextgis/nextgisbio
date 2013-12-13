dfe66a006f8f6544f12ee811545ce7d98a8d7d5d
karavanjo <jownkiv@gmail.com>
2013-12-13 07:27:52 +0300

    were changed some urls

--- eco/static/js/ugrabio/TaxonCbTree.js
+++ eco/static/js/ugrabio/TaxonCbTree.js
@@ -110,7 +110,7 @@ define(['dojo/dom', 'dojo/on', 'dojo/topic', 'dojo/store/JsonRest', 'cbtree/Tree
 
         taxonsTree.selectTaxon = function (taxonId) {
             var tree = this,
-                getPath = xhr(application_root + '/taxon/' + taxonId + '/parent_path', {
+                getPath = xhr(application_root + '/taxon/parent_path/' + taxonId, {
                     handleAs: 'json'
                 });
             getPath.then(function (data) {
