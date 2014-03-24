5e613902cea1a2dd05f9a7db4334a6a11085790f
karavanjo <jownkiv@gmail.com>
2014-03-24 09:05:02 +0300

    JsonifyMixin: constructor was added

--- eco/utils/jsonify/jsonify.py
+++ eco/utils/jsonify/jsonify.py
@@ -4,11 +4,15 @@ import datetime
 
 
 class JsonifyMixin:
+    def __init__(self):
+        pass
+
     def as_json_dict(self, **init):
         d = dict()
         for c in self.__table__.columns:
             v = getattr(self, c.name)
-            if isinstance(v, datetime.datetime): v = v.isoformat()
+            if isinstance(v, datetime.datetime):
+                v = v.isoformat()
             d[c.name] = v
         
         for k, v in init.items():
