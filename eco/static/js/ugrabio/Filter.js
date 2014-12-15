7dcf1149eb5443cd6a051922a9c681bb4deaee1d
karavanjo <jownkiv@gmail.com>
2014-12-16 00:07:25 +0300

    #47 module for getting red book filter query string parameter was implemented

---
+++ eco/static/js/ugrabio/Filter.js
@@ -0,0 +1,46 @@
+define([
+        'dojo/_base/declare',
+        'ugrabio/QueryString'
+    ], function (declare, QueryString) {
+
+        var _filter = null;
+
+        var FilterSingleton = declare(null, {
+            constructor: function () {
+                var parameters = (new QueryString).getParameters();
+
+                if (_filter) {
+                    return _filter;
+                } else {
+                    _filter = {}
+                }
+
+                if (parameters['red_book']) {
+                    _filter.red_book = parseInt(parameters['red_book'], 10);
+                }
+            },
+
+            getFilter: function () {
+                return _filter;
+            },
+
+            getAsQueryString: function () {
+                var filterParameters = [];
+
+                for (var parameterName in _filter) {
+                    if (_filter.hasOwnProperty(parameterName)) {
+                        filterParameters.push(parameterName + '=' + _filter[parameterName]);
+                    }
+                }
+
+                return filterParameters.join('&');
+            }
+        });
+
+        if (!_instance) {
+            var _instance = new FilterSingleton();
+        }
+
+        return _instance;
+    }
+);
