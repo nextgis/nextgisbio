facfea77162ce6181657ed7c47d5864eef73d9cf
karavanjo <jownkiv@gmail.com>
2015-01-09 13:57:58 +0300

    #49 loading nodes recursively by path was implemented

--- eco/static/js/ugrabio/TaxonJsTree.js
+++ eco/static/js/ugrabio/TaxonJsTree.js
@@ -1,17 +1,20 @@
 define('ugrabio/TaxonJsTree', [
     'dojo/_base/declare',
+    'dojo/_base/lang',
     'dojo/topic',
+    'dojo/Deferred',
+    'dojo/request/xhr',
     'jstree/jstree',
     'dojo/text!./templates/SpecieTreeNodeTemplate.html',
     'mustache/mustache'
-], function (declare, topic, jstree, specieTreeNodeTemplate, mustache) {
+], function (declare, lang, topic, Deferred, xhr, jstree, specieTreeNodeTemplate, mustache) {
     return declare([], {
         $taxonJsTree: null,
 
         constructor: function (domSelector) {
-            var context = this;
-            this.$taxonJsTree = jQuery(domSelector);
-            var c = this.$taxonJsTree.jstree({
+            var context = this,
+                $tree = this.$taxonJsTree = jQuery(domSelector);
+            $tree.jstree({
                 'core': {
                     'themes': {
                         'variant': 'small'
@@ -33,8 +36,16 @@ define('ugrabio/TaxonJsTree', [
                 'plugins': ['wholerow', 'checkbox']
             });
 
-            this.$taxonJsTree.on('changed.jstree', function (e, data) {
-                topic.publish('taxon/selected/changed', context.$taxonJsTree.jstree('get_top_selected'));
+            $tree.on('changed.jstree', function (e, data) {
+                topic.publish('taxon/selected/changed', $tree.jstree('get_top_selected'));
+            });
+
+            $tree.on('loaded.jstree', function (e, data) {
+                $tree.jstree('open_node', 'root');
+            });
+
+            topic.subscribe('taxon/select', function () {
+                context.selectTaxonNode(arguments[0]);
             });
         },
 
@@ -42,7 +53,7 @@ define('ugrabio/TaxonJsTree', [
         createHtmlNodes: function (data) {
             var taxonItem;
 
-            for(var i = 0, countTaxons = data.length; i < countTaxons; i++) {
+            for (var i = 0, countTaxons = data.length; i < countTaxons; i++) {
                 taxonItem = data[i];
                 if (taxonItem.is_specie && taxonItem.author) {
                     taxonItem.text = mustache.render(this.specieNodeTemplate, {
@@ -51,6 +62,52 @@ define('ugrabio/TaxonJsTree', [
                     });
                 }
             }
+        },
+
+        selectTaxonNode: function (taxonId) {
+            var tree = this.$taxonJsTree,
+                getPath = xhr(application_root + '/taxon/path/' + taxonId, {
+                    handleAs: 'json'
+                }),
+                deferred = new Deferred();
+
+            if (tree.jstree('is_loaded', taxonId)) {
+
+            }
+
+            getPath.then(lang.hitch(this, function (data) {
+                this._expandBranchByHierarchy(data.path, data.path.length, deferred);
+            }));
+
+            return deferred.promise;
+        },
+
+        _expandBranchByHierarchy: function (hierarchyPathArray, hierarchyDepth, deferred) {
+            var tree = this.$taxonJsTree,
+                currentNodeId = hierarchyPathArray[0],
+                node;
+
+            tree.jstree('load_node', currentNodeId, lang.hitch(this, function (data) {
+                hierarchyPathArray.splice(0, 1);
+                hierarchyDepth--;
+
+                if (hierarchyDepth > 1) {
+                    this._expandBranchByHierarchy(hierarchyPathArray, hierarchyDepth, deferred);
+                } else {
+                    node = tree.jstree('get_node', hierarchyPathArray[0]);
+                    if (!node.state.selected) {
+                        tree.jstree('select_node', node);
+                    }
+                    this._focusToNode(node);
+                    deferred.resolve();
+                }
+            }));
+        },
+
+        _focusToNode: function (node) {
+            // from https://github.com/vakata/jstree/issues/519
+            var tree = this.$taxonJsTree;
+            tree.scrollTop = jQuery(node).offset().top - jQuery(tree).offset().top / 2;
         }
     });
 });
