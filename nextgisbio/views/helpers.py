365c5109e311c6071aa86bc16c02205435ef7e84
Ivan Kavaliou <jownkiv@gmail.com>
2016-03-28 23:17:16 +0300

    #61 helpers for jtable were implemented

--- nextgisbio/views/helpers.py
+++ nextgisbio/views/helpers.py
@@ -1,3 +1,8 @@
+# encoding: utf-8
+
+from nextgisbio.models import table_by_name
+
+
 def get_paging_params(request_params):
     start, count = None, None
     if ('start' in request_params) and request_params['start'].isdigit() and \
@@ -14,3 +19,21 @@ def get_parsed_search_attr(request_params, attr_name='name'):
         if '%' not in parsed_name:
             parsed_name = ''.join([parsed_name, '%'])
     return parsed_name
+
+
+def get_jtable_paging_params(request_params):
+    start, count = None, None
+    if ('jtStartIndex' in request_params) and request_params['jtStartIndex'].isdigit() and \
+            ('jtPageSize' in request_params) and request_params['jtPageSize'].isdigit():
+        start = int(request_params['jtStartIndex'])
+        count = int(request_params['jtPageSize'])
+    return start, count
+
+
+def get_table_by_name(request):
+    table_name = request.matchdict['table']
+    try:
+        table = table_by_name(table_name)
+    except KeyError:
+        return {'success': False, 'msg': 'Ошибка: отсутствует таблица с указанным именем'}
+    return table, table_name
